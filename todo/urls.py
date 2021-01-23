c8b68d098a4ec273eef49fdf776f7de686fc7e05
Kylychbek <kochaj@mail.ru>
2021-01-23 20:53:23 +0600

    added second page and url

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test
+from main.views import homepage, test, second
 
 
 
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
     path("test/", test, name="test"),
+    path("test2/", second)
 ]
